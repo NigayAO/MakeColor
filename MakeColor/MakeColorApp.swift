6dcbc637515896fc840d9c3ac56e0618b0bc14dd
Alik <Nigay_AO@bk.ru>
2022-01-24 11:36:43 +0300

    Initial Commit

---
+++ MakeColor/MakeColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeColorApp.swift
+//  MakeColor
+//
+//  Created by Alik Nigay on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct MakeColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
